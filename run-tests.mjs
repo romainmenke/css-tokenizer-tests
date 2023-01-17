00f372e7d4f278f2c5839c762af69451aee1cca7
Romain Menke <romainmenke@gmail.com>
2023-01-17 18:05:53 +0100

    url tests

--- run-tests.mjs
+++ run-tests.mjs
@@ -41,39 +41,39 @@ import { traverseDir } from "./lib/traverse-dir.mjs";
 	await resultsAsHTML(results, 'index.html');
 }
 
-{
-	const testGroups = new Map();
-	const tests = new Set();
-	const testFiles = new Set(await traverseDir('./tests-community'));
+// {
+// 	const testGroups = new Map();
+// 	const tests = new Set();
+// 	const testFiles = new Set(await traverseDir('./tests-community'));
 
-	for (const testFile of testFiles) {
-		const testFileParsed = path.parse(testFile);
-		const testDirParsed = path.parse(testFileParsed.dir);
-		const testGroupName = path.parse(testDirParsed.dir).base;
-		const test = path.join(testGroupName, testDirParsed.base);
+// 	for (const testFile of testFiles) {
+// 		const testFileParsed = path.parse(testFile);
+// 		const testDirParsed = path.parse(testFileParsed.dir);
+// 		const testGroupName = path.parse(testDirParsed.dir).base;
+// 		const test = path.join(testGroupName, testDirParsed.base);
 
-		{
-			if (tests.has(test)) {
-				continue;
-			}
+// 		{
+// 			if (tests.has(test)) {
+// 				continue;
+// 			}
 
-			tests.add(test);
-		}
+// 			tests.add(test);
+// 		}
 
-		if (
-			testFiles.has(path.join(testFileParsed.dir, 'source.css')) &&
-			testFiles.has(path.join(testFileParsed.dir, 'tokens.json'))
-		) {
-			const testGroup = testGroups.get(testGroupName) ?? {
-				name: testGroupName,
-				tests: []
-			};
+// 		if (
+// 			testFiles.has(path.join(testFileParsed.dir, 'source.css')) &&
+// 			testFiles.has(path.join(testFileParsed.dir, 'tokens.json'))
+// 		) {
+// 			const testGroup = testGroups.get(testGroupName) ?? {
+// 				name: testGroupName,
+// 				tests: []
+// 			};
 
-			testGroup.tests.push(test);
-			testGroups.set(testGroupName, testGroup);
-		}
-	}
+// 			testGroup.tests.push(test);
+// 			testGroups.set(testGroupName, testGroup);
+// 		}
+// 	}
 
-	const results = await testCommunityCSSSources(Array.from(testGroups.values()));
-	await resultsAsHTML(results, 'community.html');
-}
+// 	const results = await testCommunityCSSSources(Array.from(testGroups.values()));
+// 	await resultsAsHTML(results, 'community.html');
+// }
