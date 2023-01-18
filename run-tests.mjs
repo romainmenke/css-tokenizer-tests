0541ce44019e7e1b9c8934602b8ee156006a13f2
Romain Menke <romainmenke@gmail.com>
2023-01-18 19:37:31 +0100

    add minimal tests with token type stream only

--- run-tests.mjs
+++ run-tests.mjs
@@ -44,6 +44,43 @@ import { traverseDir } from "./lib/traverse-dir.mjs";
 {
 	const testGroups = new Map();
 	const tests = new Set();
+	const testFiles = new Set(await traverseDir('./tests'));
+
+	for (const testFile of testFiles) {
+		const testFileParsed = path.parse(testFile);
+		const testDirParsed = path.parse(testFileParsed.dir);
+		const testGroupName = path.parse(testDirParsed.dir).base;
+		const test = path.join(testGroupName, testDirParsed.base);
+
+		{
+			if (tests.has(test)) {
+				continue;
+			}
+
+			tests.add(test);
+		}
+
+		if (
+			testFiles.has(path.join(testFileParsed.dir, 'source.css')) &&
+			testFiles.has(path.join(testFileParsed.dir, 'tokens.json'))
+		) {
+			const testGroup = testGroups.get(testGroupName) ?? {
+				name: testGroupName,
+				tests: []
+			};
+
+			testGroup.tests.push(test);
+			testGroups.set(testGroupName, testGroup);
+		}
+	}
+
+	const results = await testFeatures(Array.from(testGroups.values()), true);
+	await resultsAsHTML(results, 'index-minimal.html');
+}
+
+{
+	const testGroups = new Map();
+	const tests = new Set();
 	const testFiles = new Set(await traverseDir('./tests-community'));
 
 	for (const testFile of testFiles) {
